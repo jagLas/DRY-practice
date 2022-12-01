5d0ec4ff655f53b516db17909055a1d05804c71a
jagLas <jglasher@gmail.com>
2022-12-01 14:49:02 -0500

    refactored to by DRY

--- practice.js
+++ practice.js
@@ -1,47 +1,44 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  return bigSmallSelect(num1, num2, 'big') * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return bigSmallSelect(num1, num2, 'big') / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  let most = bigSmallSelect(sum1, sum2, 'big')
+  return `I ate ${most} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  let smallDog = bigSmallSelect(weight1, weight2, 'small');
+  return `I adopted a dog that weighs ${smallDog} pounds.`;
+
 }
 
+function bigSmallSelect(num1, num2, size) {
+  if (size != 'big' && size != 'small') {
+    throw 'Third argument must be either "big" or "small"'
+  }
+
+  let big;
+  let small;
+  
+  if(num1 > num2){
+    big = num1;
+    small = num2;
+  } else {
+    big = num2;
+    small = num1;
+  }
+
+  if (size === 'big') {
+    return big;
+  } else {
+    return small;
+  }
+}
 
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
